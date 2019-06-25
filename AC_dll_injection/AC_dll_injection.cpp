e19f9ee814d72e7b9b97a5158b5a747c4ad65db9
Cyriaque Pin <pincyriaque@gmail.com>
2019-06-25 21:18:57 +0200

    injection dll

--- AC_dll_injection/AC_dll_injection.cpp
+++ AC_dll_injection/AC_dll_injection.cpp
@@ -1,20 +1,67 @@
-// AC_dll_injection.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
 #include <iostream>
+#include <windows.h>
+#include <tlhelp32.h>
+
+void LoadDll(HANDLE process, const wchar_t* dllPath)
+{
+	// write the dll name to memory
+	int namelen = wcslen(dllPath) + 1;
+	LPVOID remoteString = VirtualAllocEx(process, NULL, namelen * 2, MEM_COMMIT, PAGE_EXECUTE);
+	WriteProcessMemory(process, remoteString, dllPath, namelen * 2, NULL);
+
+	// get the address of LoadLibraryW()
+	HMODULE k32 = GetModuleHandleA("kernel32.dll");
+	LPVOID funcAdr = GetProcAddress(k32, "LoadLibraryW");
+
+	// create the thread
+	HANDLE thread =
+		CreateRemoteThread(process, NULL, NULL, (LPTHREAD_START_ROUTINE)funcAdr, remoteString, NULL, NULL);
+
+	// let the thread finish and clean up
+	WaitForSingleObject(thread, INFINITE);
+	CloseHandle(thread);
+}
+
+DWORD getPIDByName(std::wstring name)
+{
+	DWORD PID = -1;
+
+	PROCESSENTRY32 entry;
+	entry.dwSize = sizeof(PROCESSENTRY32);
+
+	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+
+	if (Process32First(snapshot, &entry) == TRUE)
+	{
+		while (Process32Next(snapshot, &entry) == TRUE)
+		{
+			std::wstring binaryPath = entry.szExeFile;
+			if (binaryPath.find(name) != std::wstring::npos)
+			{
+				PID = entry.th32ProcessID;
+				break;
+			}
+		}
+	}
+
+	CloseHandle(snapshot);
+	return PID;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	DWORD pid = getPIDByName(L"ac_client.exe");
+	HANDLE process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+
+	if (process == NULL) {
+		std::cout << "Open process failed\n";
+	}
+	else {
+		std::cout << "Sucess, pid=" << pid << '\n';
+		LoadDll(process, L"..\\..\\ACDLL\\Debug\\\ACDLL.dll");
+	}
+
+	CloseHandle(process);
+    
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
